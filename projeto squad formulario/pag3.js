4b50fe389de29dda89ae2e4d9c3aff45d2eddba9
ThiagoDuart4 <106554492+ThiagoDuart4@users.noreply.github.com>
2022-09-26 18:52:32 -0300

    Quem Somos
    Parte inicial da pagina Quem somos, implementei header e suas estilizaçoes, na main os cards, e footer o copy

---
+++ projeto squad formulario/pag3.js
@@ -0,0 +1,8 @@
+// // side-bar
+// var btnMobile =  document.getElementById('#sub-btn')
+
+// function toggleMenu() {
+//    var nav = document.getElementById('#nav-menu-lateral');
+//    nav.classList.toggle('active')
+// }
+
