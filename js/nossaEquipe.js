bb561b35319f2228b18948b759d54f6c886cb5ab
letdayy <108416308+letdayy@users.noreply.github.com>
2022-09-27 11:58:08 -0300

    Delete nossaEquipe.js

--- js/nossaEquipe.js
+++
@@ -1,8 +0,0 @@
-// // side-bar
-// var btnMobile =  document.getElementById('#sub-btn')
-
-// function toggleMenu() {
-//    var nav = document.getElementById('#nav-menu-lateral');
-//    nav.classList.toggle('active')
-// }
-
