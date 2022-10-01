391a50299b028445290f26ef9d74024b8ea05a98
Thiago Duarte <106554492+ThiagoDuart4@users.noreply.github.com>
2022-10-01 18:20:56 -0300

    Implementando menu em todos as pag

--- js/batak.js
+++ js/batak.js
@@ -27,3 +27,16 @@ function prev(){
 }
 
 setInterval(next, 4000);
+
+
+// menu lateral
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
